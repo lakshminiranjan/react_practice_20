85b612df8c89f1ced4d46c09d4194cc8347af310
19BCE0590-Niranjan <lakshminiranjanp@gmail.com>
2024-03-12 16:33:25 +0530

    initial commit

--- sample_proj/src/components/MyComponent.jsx
+++ sample_proj/src/components/MyComponent.jsx
@@ -1,46 +1,27 @@
-//updater function = A function passed as an argument to setState() usaully ex.setYear(arrow function)
-//Allow for safe updates based on the previous state used with multiple state updates and asynchronous functions .GOod practice to use updater Function
 import React,{useState} from 'react'
 
 function MyComponent() {
 
-    const [count, setCount] =useState(0);
 
-    const increment =() => {
+  const [car,setCar] = useState({year:2024,make:"FOrd",model:"Mustang"});
 
-
-      //uses the current state to calculate the Next state
-      //set functions do not trigger an update
-      //React batches together state updates for performance reasons
-      //Next state becmes the current state after an update
-
-      //Takes the PENDING state to calculate the next state
-      //React puts your updater function in a queue(waiting in line)
-      //During the next render,it will call them in the same order
-
-        setCount(c => c + 1);
-        setCount(c => c + 1);
-        setCount(c => c + 1);
-    }
-
-    const decrement =() => {
-        setCount(c => c - 1);
-        setCount(c => c - 1);
-        setCount(c => c - 1);
-    }
-    const reset =() => {
-        setCount(c => c =0);
-    }
-
-
-
+  function handleYearChange(event){
+    setCar(c => ({...c,year:event.target.value}));
+  }
+    function handleMakeChange(event){
+      setCar(c => ({...c,make:event.target.value}));
+    
+  }
+    function handleModelChange(event){
+    setCar(c => ({...c,model:event.target.value}))
+  }
 
   return (
-    <div className="counter-container">
-        <p className="count-display">{count}</p>
-        <button className="counter-button" onClick={decrement}>Decrement</button>
-        <button className="counter-button" onClick={reset}>Reset</button>
-        <button className="counter-button" onClick={increment}>Increment</button>
+    <div>
+      <p>Your fauvorite car is : {car.year} {car.make} {car.model}</p>
+      <input type="number" value={car.year}   onChange={handleYearChange}/><br />
+      <input type="text" value={car.make}  onChange={handleMakeChange}/><br />
+      <input type="text" value={car.model}  onChange={handleModelChange}/><br />
     </div>
   )
 }
